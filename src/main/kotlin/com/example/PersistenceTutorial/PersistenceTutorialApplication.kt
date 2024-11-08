e39f0e169577abcd3178f10018218c5f05acf2da
badbu@GreenBeast <mtodd@muze3d.graphics>
2024-11-08 15:07:40 -0500

    Get Database Entry by Player ID(PID)

--- src/main/kotlin/com/example/PersistenceTutorial/PersistenceTutorialApplication.kt
+++ src/main/kotlin/com/example/PersistenceTutorial/PersistenceTutorialApplication.kt
@@ -7,6 +7,8 @@ import jakarta.persistence.Id
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
@@ -18,17 +20,24 @@ fun main(args: Array<String>) {
 
 interface PlayerDataRepository: JpaRepository<playerdata, Long>
 {
+	@Query(value = "Call GetByPID(:PID)", nativeQuery = true)
+	fun GetByPID(@Param("PID") PID: Int): playerdata
 
 }
 
 @RestController
 @RequestMapping("api")
-
-
 class PlayerDataRestController(val PlayerDataRepo: PlayerDataRepository)
 {
 	@GetMapping("PlayerData") // controls where the map goes localhost:8080/Example
 	fun GetAll() = PlayerDataRepo.findAll()
+
+	@GetMapping("PlayerData/{PID}")  //custom Mapping to get Player by PID
+	fun GetPID(@PathVariable(value = "PID") PID: Int) : playerdata
+	{
+		var PData = PlayerDataRepo.GetByPID(PID)
+		return PData
+	}
 
 	@PostMapping("PlayerData")
 	fun SavePlayerData(@RequestBody PlayerData: playerdata)
@@ -40,7 +49,7 @@ class PlayerDataRestController(val PlayerDataRepo: PlayerDataRepository)
 @Entity
 class playerdata(
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	val Id: Long = 0, var isvalid: Boolean = false, var XCoord: Float = 0.0f, var YCoord: Float = 0.0f, var ZCoord: Float = 0.0f
+	val Id: Long = 0, var isvalid: Boolean = false, var pid: Int = -1, var XCoord: Float = 0.0f, var YCoord: Float = 0.0f, var ZCoord: Float = 0.0f
 )
 
 
